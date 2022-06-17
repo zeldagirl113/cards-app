e538bfbae41ac5f8ef7519a676fb705ce750fe57
tgreelz <tgreeley@umass.edu>
2022-06-16 22:30:16 -0400

    Basic Layout of Home Page

---
+++ pixel-cards/src/Pages/First.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class First extends Component {
+    render() {
+        return(
+            <p>This is a test of the first page</p>
+        );
+    }
+} 
